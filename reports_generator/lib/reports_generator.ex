805ab4aca88aeefec984fa17062d1c2173599dff
Wilson Neto <wilsonpsn@hotmail.com>
2021-04-12 09:34:12 -0300

    Improvement: Added guards

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -12,15 +12,20 @@ defmodule ReportsGenerator do
     "pizza"
   ]
 
+  @options ["foods", "users"]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
     |> Enum.reduce(report_acc(), &sum_values/2)
   end
 
-  def fetch_higher_cost(report, option), do: Enum.max_by(report[option], fn {_key, value} -> value end)
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option!"}
 
-  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users } = report) do
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
     %{report | "users" => users, "foods" => foods}
